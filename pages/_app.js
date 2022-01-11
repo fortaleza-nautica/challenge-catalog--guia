3f843f1d8d0a4f00199a9c433a21a395043d33c3
sarah-lima <sarah.lima@bandtec.com.br>
2022-01-11 08:02:00 -0300

    finalização de desafio

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import '../styles/globals.css';
+import '../styles/index.css';
+
+function MyApp({ Component, pageProps }) {
+    return (
+            <div className="container mx-auto my-6">
+                <Component {...pageProps} />
+            </div>
+    );
+}
+
+export default MyApp;
